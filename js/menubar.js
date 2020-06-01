52ccfa544b54b640c504cd71ad381be6467f7af0
HanhPham <44548605+HanhPhamHus@users.noreply.github.com>
2020-06-01 10:02:13 +0700

    Add files via upload

---
+++ js/menubar.js
@@ -0,0 +1,18 @@
+var open 			= document.getElementById("btn-open-bar");
+var nav 			= document.getElementById("nav");
+
+open.addEventListener("click", ControlNav);
+var numControlNav = 0;
+
+function ControlNav() {
+   if(numControlNav == 0){
+      console.log(numControlNav);
+      nav.style.display = "block";
+      numControlNav = 1;
+   }else{
+      
+      console.log(numControlNav);
+      nav.style.display = "none";
+      numControlNav = 0;
+   }
+}
